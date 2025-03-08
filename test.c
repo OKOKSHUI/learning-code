02a99f88ce67a89c029ebe73dc3a101de6488ed1
okokshui <1701779878@qq.com>
2025-03-08 14:57:07 +0800

    第一次尝试提交

---
+++ test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main()
+{
+
+    printf("Yeah!Yeah!This is my first learning-code\n");
+    int arr[8] = {1,2,3,4,5,6,7,8};
+    for(int i = 0; i < 8 ; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
